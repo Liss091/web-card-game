e7e982509d727c156be49e3f5d3745be6f12c715
DianaLysenko <42473300+Liss091@users.noreply.github.com>
2019-01-08 18:03:15 +0200

    Test for ResourceDaoImpl

---
+++ src/test/java/ua/od/game/repository/dao/impl/ResourceDaoImplTest.java
@@ -0,0 +1,32 @@
+package ua.od.game.repository.dao.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.od.game.model.ResourceEntity;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class ResourceDaoImplTest {
+
+    ResourceDaoImpl resourceDao;
+
+    @Before
+    public void init() {
+        resourceDao = new ResourceDaoImpl();
+    }
+
+    @Test
+    public void getResourceListTest() {
+        List<ResourceEntity> resources = resourceDao.getAllResourceList();
+        for (int i = 0; i < resources.size(); i++) {
+            System.out.print(resources.get(i).getId() + " ");
+            System.out.print(resources.get(i).getName() + " ");
+            System.out.print(resources.get(i).getDescription()+ " ");
+            System.out.print(resources.get(i).getDefaultNumber()+ " ");
+        }
+        assertTrue(!resources.isEmpty());
+    }
+
+}
