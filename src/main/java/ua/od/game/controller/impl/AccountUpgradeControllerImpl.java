9cd012eb0d8a7d879e34de3740b5dc22cbba275b
Vladimir Plentiov <deadlight967@gmail.com>
2019-02-07 13:19:00 +0200

    test

--- src/main/java/ua/od/game/controller/impl/AccountUpgradeControllerImpl.java
+++ src/main/java/ua/od/game/controller/impl/AccountUpgradeControllerImpl.java
@@ -5,22 +5,29 @@ import ua.od.game.dto.AccountUpgradeDto;
 import ua.od.game.service.AccountUpgradeService;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
+@Path("/account")
 public class AccountUpgradeControllerImpl implements AccountUpgradeController {
     @Inject
     public AccountUpgradeService accountUpgradeService;
 
     @Override
-
-    public Response clearAccountUpgradeList(Integer accountId) {
-        accountUpgradeService.clearAccountUpgradeList(accountId);
-        return null;
+    @GET
+    @Path("/{accountId}")
+    public Response clearAccountUpgradeList(@PathParam("accountId") Integer accountId) {
+        if (accountUpgradeService.clearAccountUpgradeList(accountId)) return Response.status(200).build();
+        else return Response.status(500).build();
     }
 
     @Override
-    public List<AccountUpgradeDto> getAccountUpgradeList(Integer accountId) {
+    @GET
+    @Path("/upgrade/{accountId}")
+    public List<AccountUpgradeDto> getAccountUpgradeList(@PathParam("accountId") Integer accountId) {
         return accountUpgradeService.getAccountUpgradeList(accountId);
     }
 }
